4a3f277cd70a38f34b5fb24835ea640fa42ad41f
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-08-05 16:37:57 +0200

    Intial Component - parameter

---
+++ BlazorAppDemo/Client/Model/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppDemo.Client.Model
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleasDate { get; set; }
+    }
+}
