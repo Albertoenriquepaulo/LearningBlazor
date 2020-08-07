a3b6176fef6dcbbc7cb305c3426fab71d7cd9495
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-08-07 18:44:04 +0200

    13. Invoking JavaScript Functions From C#

---
+++ BlazorAppDemo/Client/Helpers/IJSRuntimeExtensionMethodsXX.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppDemo.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethodsXX
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("console.log", "Example of InvokeVoidAsync");
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static async ValueTask MyFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("myFunction", message);
+        }
+    }
+}
