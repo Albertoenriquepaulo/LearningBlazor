6914499700012ba8b0f56fcdcad8118bd4e2b421
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-08-28 22:48:22 +0200

    9. Building a Multiple Selector Component

--- BlazorAppDemo/Client/Model/Movie.cs
+++ BlazorAppDemo/Client/Model/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace BlazorAppDemo.Client.Model
 {
     public class Movie
     {
-        public int Id { get; set; } = 1;
+        public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
-        public DateTime ReleasDate { get; set; }
+        public string Summary { get; set; }
+        public bool InTheaters { get; set; }
+        public string Trailer { get; set; }
+
+        [Required]
+        public DateTime? ReleasDate { get; set; }
         public string Poster { get; set; }
         public string TitleBrief
         {
